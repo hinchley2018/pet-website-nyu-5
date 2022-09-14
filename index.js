b519899d539b038fdde07b670d131bf2c3f8167f
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-09-14 18:09:24 -0500

    add first js examples

---
+++ index.js
@@ -0,0 +1,23 @@
+// This is a single commment
+
+/*
+anything 
+in h
+here 
+is a commented out
+*/
+
+// print something to develoepr console
+console.log("Hello from JS file")
+
+
+//declare a variable and assign to that varaibles
+var countCandyCanes = 10
+
+// print this out
+console.log("We earned $", .50* countCandyCanes, "dollars this season", "we sold", countCandyCanes, "candy canes")
+
+
+console.log("We're going to the store")
+//we can use single quotes but might have to escape them if used in text
+console.log('We\'re going to the store')
